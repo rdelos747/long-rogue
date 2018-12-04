2a57d643e5ce6228cb2c7613553a779e07254f61
rafael de los santos <raf@rafaels-MBP.home>
2018-12-04 00:53:01 -0500

    added menu button

--- long rogue/bottom_hud/bottom_hud.swift
+++ long rogue/bottom_hud/bottom_hud.swift
@@ -17,20 +17,20 @@ class BottomHud: SKSpriteNode {
     let hpBox:StatusBox
     let shBox:StatusBox
     let pwBox:StatusBox
+    let menuBox:MenuBox
     
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
     init(_ x:CGFloat, _ y:CGFloat, _ w:CGFloat, _ h:CGFloat) {
-        let statusBoxWidth:CGFloat = w / 4
         let statusBoxMargin:CGFloat = 10
+        let statusBoxWidth:CGFloat = (w - statusBoxMargin * 4) / 4
         let statusBoxHeight:CGFloat = 40
         let statusBoxBottom:CGFloat = 20
         
         let logBoxOffset:CGFloat = 10
         let logBoxHeight:CGFloat = h - (statusBoxBottom + statusBoxHeight + (logBoxOffset * 2))
-        print(h)
         
         self.logBox = Log(
             10,
@@ -66,6 +66,12 @@ class BottomHud: SKSpriteNode {
             statusBoxWidth,
             statusBoxHeight
         )
+        self.menuBox = MenuBox(
+            (statusBoxMargin * 4) + (statusBoxWidth * 3),
+            statusBoxBottom,
+            (statusBoxWidth - statusBoxMargin),
+            statusBoxHeight
+        )
         
         //init self
         super.init(texture:nil, color:hex(HUD_COLOR), size:CGSize(width: w, height: h))
@@ -76,6 +82,7 @@ class BottomHud: SKSpriteNode {
         self.addChild(self.hpBox)
         self.addChild(self.shBox)
         self.addChild(self.pwBox)
+        self.addChild(self.menuBox)
     }
     
     func getText(_ text:String) {
